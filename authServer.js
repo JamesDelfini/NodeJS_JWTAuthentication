a5addccf67d9d4e98d1c6b96823f8a9394a292a3
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-05 23:05:51 +0800

    Deauthenticate user tokens using the logout function

--- authServer.js
+++ authServer.js
@@ -12,7 +12,7 @@ app.post('/token', (req, res) => {
     const refreshToken = req.body.token
 
     if (refreshToken == null) return res.sendStatus(401);
-    if (!refreshToken.includes(refreshToken)) return res.sendStatus(403);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403)
@@ -33,6 +33,11 @@ app.post('/login', (req, res) => {
 
     refreshTokens.push(refreshToken);
     res.json({accessToken, refreshToken});
+});
+
+app.delete('/logout', (req, res) => {
+   refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+   res.sendStatus(204); 
 });
 
 function generateAccessToken(user){
